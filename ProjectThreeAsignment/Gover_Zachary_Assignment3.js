3b1d084a9721c2ae418ddceb7c0afe702c9f8110
Zachary1748 <zachary1748@fullsail.edu>
2014-10-16 13:20:17 -0700

    Fixed bug
    Fixed bug; return value for jobOffer.
    Added another function.

--- ProjectThreeAsignment/Gover_Zachary_Assignment3.js
+++ ProjectThreeAsignment/Gover_Zachary_Assignment3.js
@@ -7,49 +7,87 @@
 
 var acceptedOffer = true;
 var hirePeople = true;
-var hireAmmount = 4;
+var hireAmmount;
 
 var jobOffer = prompt("We have an offer from Freelancer.com to build a big eCommerce website, do you want to accept it?", "Yes or No?");
 
 // Procedure
-var codingLangauge = function() {
+var requestAnswer = function() {
     
-    // Local Variables
     var codingCMS = "Joomla";
     
-    // Conditional Statement
     if (jobOffer === "Yes" || jobOffer === "yes") {
         
-        // Nested Conditional Statement
-        if (codingCMS == "Joomla") {
-            
-            console.log("Awesome! Let's get started on the project.");
-            console.log("Next we need to find out how many people we need to hire for this project.");
-            console.log("Also we have decided that we are going to build this eCommerce website with " + codingCMS);
-            
-            return codingCMS
-            
-        } else {
-            
-            console.log("Oop's there was an error. We were supposed to be building this website using Joomla.");
-            
-        }
+        console.log("Awesome! Let's get started on the project.");
+        console.log("Next we need to find out how many people we need to hire for this project.");
+        console.log("Also we have decided that we are going to build this eCommerce website with " + codingCMS);
+        
+        return codingCMS;
         
     } else {
         
-        alert("Dang! We\'ll find another offer and let you know boss.");
+        console.log("Oop\'s something went wrong.");
+        
+        return jobOffer;
         
     }
     
-    return codingCMS;
+};
+
+// Function is called when user just can't the hireAmmount just right
+var hireIncorrect = function () {
+    
+    console.log("Alright well you couldn't get the hire ammount just right so we will hire 3 PHP expert\'s and 2 JavaScript expert\'s people");
+        
+    var jsExpert = 2;
+    
+    var phpExpert = 3;
+    
+    var hireAmmount = jsExpert + phpExpert;
+    
+    return hireAmmount;
     
 }
-
-codingLangauge();
 
 if (jobOffer === "Yes" || jobOffer === "yes") {
     
-    confirm("We have accepted the project offer and we are going to build it using " + codingCMS);
+    console.log("Hello " + requestAnswer());
+
+    hireAmmount = prompt("How many people would you like to hire for this project?", "1-10");
+    
+    while (hireAmmount < 1 || hireAmmount > 10) {
+        
+        for (hireAmmount = hireAmmount; hireAmmount > 10; hireAmmount--) {
+            
+            hireAmmount = prompt("Too high! Try again, how many people would you like to hire for this project?", "1-10");
+            
+        }
+        
+        for (hireAmmount = hireAmmount; hireAmmount < 1; hireAmmount++) {
+            
+            hireAmmount = prompt("Too low! Try again, how many people would you like to hire for this project?", "1-10");
+            
+        }
+        
+        hireIncorrect();
+        
+        hireAmmount = 5;
+        
+        break;
+        
+    }
+    
+    
+    console.log(hireAmmount);
+    
+    
+} else {
+    
+    console.log("Dang! We\'ll find another job boss.");
     
 }
+
+
+
+
 
