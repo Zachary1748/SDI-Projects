a77f0edcd749e5e0630fd9c645f54ca5ad3a0d78
Zachary1748 <zachary1748@fullsail.edu>
2014-10-22 21:59:03 -0700

    First Commit(Project 4)
    1 Function; Check it web address starts with ‘http://’ or ‘https://’

---
+++ ProjectFourAssignment/gover_zachary_assignment4.js
@@ -0,0 +1,27 @@
+// Project: Project 4
+// Name: Zachary Gover
+// Student Number: 0003013517
+// Date: October 23rd, 2014
+// Term Number: 1410
+// Course: SDI
+
+var webAddress = function (){
+    
+    var address = "http://goverdevelopment.com";
+    var end = address.indexOf("g");
+    var withHttp = address.substring(0, end);
+    
+    if (withHttp == "http://" || withHttp == "https://") {
+        
+        console.log("var address is " + address + "." + " That is the correct format.");
+        
+    } else {
+        
+        console.log("var address is " + address + " which is not correct, the correct format would start out with https://.");
+        
+    }
+    
+    return address;
+    
+};
+
