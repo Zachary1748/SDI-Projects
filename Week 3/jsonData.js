f9c128edd795153cf7a25596fd68dc8e9449a91e
Zachary1748 <zachary1748@fullsail.edu>
2014-12-11 15:15:32 -0800

    Project 3 JSON Data

---
+++ Week 3/jsonData.js
@@ -0,0 +1,70 @@
+var json1 = {
+    
+    "leadEmployees": [
+        
+        {
+            
+            "employeename" : "Zachary Gover",
+            "position" : "Co-Founder",
+            "age" : 19
+            
+        },
+        {
+            
+            "employeename" : "Stephanie Sherfield",
+            "position" : "Co-Founder",
+            "age" : 19
+            
+        }
+        
+    ]
+    
+};
+
+var json2 = {
+    
+    "regularEmployees": [
+        
+        {
+            
+            "employeename" : "Joe Mart",
+            "position" : "Lead Develop",
+            "age" : 43
+            
+        },
+        {
+            
+            "employeename" : "Joshua Demoran",
+            "position" : "Front-End Developer",
+            "age" : 30
+            
+        },
+        {
+            
+            "employeename" : "Kristine Sharp",
+            "position" : "Back-End Developer",
+            "age" : 24
+            
+        },
+        {
+            
+            "employeename" : "Mark Darts",
+            "position" : "Project Managment",
+            "age" : 32
+            
+        }
+        
+    ]
+    
+};
+
+
+
+
+
+
+
+
+
+
+
