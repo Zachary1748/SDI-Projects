881eb82fe6c79b4bb35fad9e6391aad52abba871
Zachary1748 <zachary1748@fullsail.edu>
2014-12-18 01:10:02 -0800

    Second Commit; Project 4
    Added check number pattern function

--- Week 4/Gover_Zachary_Assignment4.js
+++ Week 4/Gover_Zachary_Assignment4.js
@@ -3,3 +3,38 @@
 // Zachary Gover
 // 0003013517
 // December 17th, 2014
+
+// Global Variables
+var phoneNumber = "000-000-0000";
+
+// Functions
+var checkNumber = function(number) {
+    
+    var firstThree = number.charAt(3);
+    var secondThree = number.charAt(7);
+    var correctLength = number.length;
+    
+    if (firstThree == "-" && secondThree == "-" && correctLength === 12) {
+        
+        console.log("Correct!");
+        return number;
+        
+    } else {
+        
+        console.log("INCORRECT! Please try again!");
+        return false;
+        
+    }
+    
+};
+
+
+// Main Code
+// Check number pattern
+var number = checkNumber(phoneNumber);
+
+if (number != false) {
+    
+    console.log("var checkNumber() equals: " + number);
+
+}
